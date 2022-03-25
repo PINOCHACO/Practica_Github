8314536ffa442427b2a0aae4b46c8df645262043
Baabiii <barby.suarez15@gmail.com>
2022-03-25 10:37:59 +0100

    comentarios

--- conexion_bbdd.php
+++ conexion_bbdd.php
@@ -1,8 +1,10 @@
 <?php
+//HACEMOS LA CONEXION A LA BASE DE DATOS
 $cadena_conexion = 'mysql:dbname=grupo_5;host=132.226.219.27';
 $usuario = 'grupo_5';
 $clave = 'afa_8175';
 $bd = new PDO($cadena_conexion, $usuario, $clave);
 
+//SELECCIONAMOS TODOS LOS DATOS DE LA TABLA
 $sql = "SELECT * FROM users";
 $users = $bd->query($sql);
