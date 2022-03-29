52a0d3ebbef24880d54dd866407c966ecc15062c
PINOCHACO <therevoltionfire@gmail.com>
2022-03-29 09:24:34 +0200

    css

--- vista.php
+++ vista.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="estilos.css">
     <title>Vista Usuarios</title>
+
+
 </head>
 
 <body>
 
     <div class="container">
-
+        <h1>Usuarios</h1>
         <?php
         include 'conexion_bbdd.php';
 
@@ -20,7 +22,7 @@
         ?>
             <div>
                 <div>
-                    <div class="row">
+                    <div>
                         <h2><?= $user['user_name'] ?></h2>
                         <p><?= $user['age'] ?></p>
                     </div>
